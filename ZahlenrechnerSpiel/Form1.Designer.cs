181f44fec4c26a816010b75246dff9a979d54701
geiert <geiert@PC7961>
2022-07-22 11:09:15 +0200

    Projektdateien hinzufügen.

---
+++ ZahlenrechnerSpiel/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace ZahlenrechnerSpiel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbversuche = new System.Windows.Forms.TextBox();
+            this.tbsumme = new System.Windows.Forms.TextBox();
+            this.tbrichtig = new System.Windows.Forms.TextBox();
+            this.tbfalsch = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbz1 = new System.Windows.Forms.TextBox();
+            this.tbz2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(110, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(202, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(18, 171);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(110, 171);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 35);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(202, 171);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 35);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(18, 212);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(86, 35);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(110, 212);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(86, 35);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(202, 212);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(86, 35);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(18, 12);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(80, 22);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Neu";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Anzahl Versuche:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Summe:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Richtig:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(301, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Falsch:";
+            // 
+            // tbversuche
+            // 
+            this.tbversuche.Location = new System.Drawing.Point(110, 40);
+            this.tbversuche.Name = "tbversuche";
+            this.tbversuche.ReadOnly = true;
+            this.tbversuche.Size = new System.Drawing.Size(65, 20);
+            this.tbversuche.TabIndex = 1;
+            // 
+            // tbsumme
+            // 
+            this.tbsumme.Location = new System.Drawing.Point(361, 138);
+            this.tbsumme.Name = "tbsumme";
+            this.tbsumme.ReadOnly = true;
+            this.tbsumme.Size = new System.Drawing.Size(135, 20);
+            this.tbsumme.TabIndex = 15;
+            // 
+            // tbrichtig
+            // 
+            this.tbrichtig.Location = new System.Drawing.Point(361, 179);
+            this.tbrichtig.Name = "tbrichtig";
+            this.tbrichtig.ReadOnly = true;
+            this.tbrichtig.Size = new System.Drawing.Size(135, 20);
+            this.tbrichtig.TabIndex = 16;
+            // 
+            // tbfalsch
+            // 
+            this.tbfalsch.Location = new System.Drawing.Point(361, 220);
+            this.tbfalsch.Name = "tbfalsch";
+            this.tbfalsch.ReadOnly = true;
+            this.tbfalsch.Size = new System.Drawing.Size(135, 20);
+            this.tbfalsch.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Deine Rechnung:";
+            this.label5.Visible = false;
+            // 
+            // tbz1
+            // 
+            this.tbz1.Location = new System.Drawing.Point(110, 85);
+            this.tbz1.Name = "tbz1";
+            this.tbz1.ReadOnly = true;
+            this.tbz1.Size = new System.Drawing.Size(65, 20);
+            this.tbz1.TabIndex = 19;
+            this.tbz1.Visible = false;
+            // 
+            // tbz2
+            // 
+            this.tbz2.Location = new System.Drawing.Point(202, 85);
+            this.tbz2.Name = "tbz2";
+            this.tbz2.ReadOnly = true;
+            this.tbz2.Size = new System.Drawing.Size(66, 20);
+            this.tbz2.TabIndex = 20;
+            this.tbz2.Visible = false;
+            this.tbz2.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(183, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "+";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(107, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(189, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Max. 5 Versuche! 10 Richtige für Sieg!";
+            this.label7.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 262);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbz2);
+            this.Controls.Add(this.tbz1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbfalsch);
+            this.Controls.Add(this.tbrichtig);
+            this.Controls.Add(this.tbsumme);
+            this.Controls.Add(this.tbversuche);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Mathespiel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbversuche;
+        private System.Windows.Forms.TextBox tbsumme;
+        private System.Windows.Forms.TextBox tbrichtig;
+        private System.Windows.Forms.TextBox tbfalsch;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbz1;
+        private System.Windows.Forms.TextBox tbz2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ ZahlenrechnerSpiel/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZahlenrechnerSpiel
+{
+    public partial class Form1 : Form
+    {
+        int versuche = 0;
+        int zahl1 = 0;
+        int zahl2 = 0;
+        int richtig = 0;
+        int falsch = 0;
+        int summe = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            //erstelle randomzahl und gebe diese bei Textfeld Summe aus!
+            Random rnd = new Random();
+            int random = rnd.Next(2, 16);
+            tbversuche.Text = versuche.ToString();
+            tbsumme.Text = random.ToString();
+            tbrichtig.Text = richtig.ToString();
+            tbfalsch.Text = falsch.ToString();
+            tbz1.Text = zahl1.ToString();
+            tbz2.Text = zahl2.ToString();
+            tbrichtig.BackColor = Color.LightGray;
+            tbfalsch.BackColor = Color.LightGray;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(zahl1 != 0)
+            {
+                zahl2 = 1;
+                tbz2.Text = zahl2.ToString();
+            }
+            else
+            {
+                zahl1 = 1;
+                tbz1.Text = zahl1.ToString();
+                tbrichtig.BackColor = Color.LightGray;
+                tbfalsch.BackColor = Color.LightGray;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (zahl1 != 0)
+            {
+                zahl2 = 2;
+                tbz2.Text = zahl2.ToString();
+            }
+            else
+            {
+                zahl1 = 2;
+                tbz1.Text = zahl1.ToString();
+                tbrichtig.BackColor = Color.LightGray;
+                tbfalsch.BackColor = Color.LightGray;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (zahl1 != 0)
+            {
+                zahl2 = 3;
+                tbz2.Text = zahl2.ToString();
+            }
+            else
+            {
+                zahl1 = 3;
+                tbz1.Text = zahl1.ToString();
+                tbrichtig.BackColor = Color.LightGray;
+                tbfalsch.BackColor = Color.LightGray;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (zahl1 != 0)
+            {
+                zahl2 = 4;
+                tbz2.Text = zahl2.ToString();
+            }
+            else
+            {
+                zahl1 = 4;
+                tbz1.Text = zahl1.ToString();
+                tbrichtig.BackColor = Color.LightGray;
+                tbfalsch.BackColor = Color.LightGray;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (zahl1 != 0)
+            {
+                zahl2 = 5;
+                tbz2.Text = zahl2.ToString();
+            }
+            else
+            {
+                zahl1 = 5;
+                tbz1.Text = zahl1.ToString();
+                tbrichtig.BackColor = Color.LightGray;
+                tbfalsch.BackColor = Color.LightGray;
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (zahl1 != 0)
+            {
+                zahl2 = 6;
+                tbz2.Text = zahl2.ToString();
+            }
+            else
+            {
+                zahl1 = 6;
+                tbz1.Text = zahl1.ToString();
+                tbrichtig.BackColor = Color.LightGray;
+                tbfalsch.BackColor = Color.LightGray;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (zahl1 != 0)
+            {
+                zahl2 =7;
+                tbz2.Text = zahl2.ToString();
+            }
+            else
+            {
+                zahl1 =7;
+                tbz1.Text = zahl1.ToString();
+                tbrichtig.BackColor = Color.LightGray;
+                tbfalsch.BackColor = Color.LightGray;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (zahl1 != 0)
+            {
+                zahl2 = 8;
+                tbz2.Text = zahl2.ToString();
+            }
+            else
+            {
+                zahl1 = 8;
+                tbz1.Text = zahl1.ToString();
+                tbrichtig.BackColor = Color.LightGray;
+                tbfalsch.BackColor = Color.LightGray;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (zahl1 != 0)
+            {
+                zahl2 = 9;
+                tbz2.Text = zahl2.ToString();
+            }
+            else
+            {
+                zahl1 = 9;
+                tbz1.Text = zahl1.ToString();
+                tbrichtig.BackColor = Color.LightGray;
+                tbfalsch.BackColor = Color.LightGray;
+            }
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            versuche = 0;
+            zahl1 = 0;
+            zahl2 = 0;
+            richtig = 0;
+            falsch = 0;
+            summe = 0;
+
+
+            tbversuche.Text = "0";
+            tbrichtig.Text = "0";
+            tbfalsch.Text = "0";
+            tbz1.Text = "0";
+            tbz2.Text = "0";
+
+            Random rnd = new Random();
+            int random = rnd.Next(2, 16);
+            tbsumme.Text = random.ToString();
+
+            tbrichtig.BackColor = Color.LightGray;
+            tbfalsch.BackColor = Color.LightGray;
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            if (tbrichtig.Text == "10")
+            {
+                DialogResult baum = MessageBox.Show("DU HAST GEWONNEN YIPPIEE *.*", "Neu Starten?", MessageBoxButtons.OK);
+                if (baum == DialogResult.OK)
+                {
+                    versuche = 0;
+                    zahl1 = 0;
+                    zahl2 = 0;
+                    richtig = 0;
+                    falsch = 0;
+                    summe = 0;
+
+
+                    tbversuche.Text = "0";
+                    tbrichtig.Text = "0";
+                    tbfalsch.Text = "0";
+                    tbz1.Text = "0";
+                    tbz2.Text = "0";
+
+                    Random rnd = new Random();
+                    int random = rnd.Next(2, 16);
+                    tbsumme.Text = random.ToString();
+
+                    tbrichtig.BackColor = Color.LightGray;
+                    tbfalsch.BackColor = Color.LightGray;
+                }
+            }
+            else if (versuche < 5)
+            {
+                if (tbz2.Text != "0")
+                {
+                    summe = zahl1 + zahl2;
+                }
+
+
+                int.TryParse(tbsumme.Text, out int Summe1);
+
+                if (tbz2.Text != "0")
+                {
+                    if (summe == Summe1)
+                    {
+                        richtig++;
+                        tbrichtig.Text = richtig.ToString();
+                        tbrichtig.BackColor = Color.Green;
+                    }
+                    else if (summe != Summe1)
+                    {
+                        falsch++;
+                        versuche++;
+
+                        tbversuche.Text = versuche.ToString();
+                        tbfalsch.Text = falsch.ToString();
+                        tbfalsch.BackColor = Color.Red;
+                    }
+                }
+
+                zahl1 = 0;
+                zahl2 = 0;
+
+                tbz1.Text = "0";
+                tbz2.Text = "0";
+
+                Random rnd = new Random();
+                int random = rnd.Next(2, 19);
+                tbsumme.Text = random.ToString();
+            }
+            else
+            {
+                DialogResult baum = MessageBox.Show("Du hast alle versuche verbraucht! Q.Q", "Neu Starten?", MessageBoxButtons.OK);
+                if (baum == DialogResult.OK)
+                {
+                    versuche = 0;
+                    zahl1 = 0;
+                    zahl2 = 0;
+                    richtig = 0;
+                    falsch = 0;
+                    summe = 0;
+
+
+                    tbversuche.Text = "0";
+                    tbrichtig.Text = "0";
+                    tbfalsch.Text = "0";
+                    tbz1.Text = "0";
+                    tbz2.Text = "0";
+
+                    Random rnd = new Random();
+                    int random = rnd.Next(2, 16);
+                    tbsumme.Text = random.ToString();
+
+                    tbrichtig.BackColor = Color.LightGray;
+                    tbfalsch.BackColor = Color.LightGray;
+                }
+            }
+        }
+    }
+}
